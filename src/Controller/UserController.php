9d48022d2d53052b2362e305f96d4ad88469d508
Imad <karimimadbz@gmail.com>
2025-07-31 09:35:37 +0200

    maj

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
@@ -22,12 +23,45 @@ final class UserController extends AbstractController
     }
 
     #[Route('admin/user/update/{id}', name: 'app_user_change_role')]
+    #[IsGranted("ROLE_ADMIN")]
     public function updateRole(EntityManagerInterface $entityManager, User $user): Response
     {
         $user->setRoles(['ROLE_EDITOR','ROLE_USER']);
         $entityManager->flush();
 
+        $this->addFlash('success', 'The user\'s role have been updated to Editor !');
+
+
         return $this->redirectToRoute('app_user');
     }
+
+    #[Route('admin/user/delete/{id}', name: 'app_user_delete_role')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function deleteRole(EntityManagerInterface $entityManager, User $user): Response
+    {
+        $user->setRoles([]);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'The user\'s role have been updated to Editor !');
+
+
+        return $this->redirectToRoute('app_user');
+    }
+
+    #[Route('admin/user/{id}/remove', name: 'app_user_remove')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function removeUser(EntityManagerInterface $entityManager, UserRepository $userRepository,$id): Response
+    {
+        $user=$userRepository->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('danger', 'The user\'s deleted!');
+
+
+        return $this->redirectToRoute('app_user');
+    }
+
+   
 
 }
